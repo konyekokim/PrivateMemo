08b207fe19c416e46f2999dbf19f679abf66d5f5
Konye <konyekokim@gmail.com>
2018-03-23 06:52:56 +0100

    send extra from GetStarted to PrivateSignIn

--- app/src/main/java/com/chokus/konye/privatememo/activity/GetStartedActivity.kt
+++ app/src/main/java/com/chokus/konye/privatememo/activity/GetStartedActivity.kt
@@ -12,6 +12,10 @@ import kotlinx.android.synthetic.main.content_get_started.*
 
 class GetStartedActivity : AppCompatActivity() {
 
+    companion object {
+        const val USER_EMAIL = "com.chokus.konye.privatememo.activity user email address"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
@@ -22,11 +26,17 @@ class GetStartedActivity : AppCompatActivity() {
     private fun viewActions(){
         sign_in_textView.setOnClickListener {
             val intent = Intent(this, PrivateRegisterActivity::class.java)
-            startActivity(intent)
+            if(get_started_email != null){
+                intent.putExtra(USER_EMAIL,get_started_email.text)
+                startActivity(intent)
+            }
         }
         forward_arrow_img.setOnClickListener {
             val intent = Intent(this, PrivateRegisterActivity::class.java)
-            startActivity(intent)
+            if(get_started_email != null){
+                intent.putExtra(USER_EMAIL,get_started_email.text)
+                startActivity(intent)
+            }
         }
     }
 
