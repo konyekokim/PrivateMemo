cb79ec465e619ec16ff5e5496ac029d8a0e14a90
Konye <konyekokim@gmail.com>
2018-03-26 07:40:30 +0100

    fix gridLayoutmanager cast

--- app/src/main/java/com/chokus/konye/privatememo/activity/NoteListActivity.kt
+++ app/src/main/java/com/chokus/konye/privatememo/activity/NoteListActivity.kt
@@ -6,6 +6,7 @@ import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.WindowManager
 import android.widget.*
