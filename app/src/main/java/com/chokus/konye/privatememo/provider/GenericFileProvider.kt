993fdc6f1b4f85f660a6238152cc2c6594cdf970
Konye <konyekokim@gmail.com>
2018-04-08 19:02:31 +0100

    add GenericFileProvider class and provider path resource and also add provider to the manifest

---
+++ app/src/main/java/com/chokus/konye/privatememo/provider/GenericFileProvider.kt
@@ -0,0 +1,10 @@
+package com.chokus.konye.privatememo.provider
+
+import android.support.v4.content.FileProvider
+
+/**
+ * Created by omen on 20/02/2018.
+ */
+class GenericFileProvider : FileProvider(){
+
+}
