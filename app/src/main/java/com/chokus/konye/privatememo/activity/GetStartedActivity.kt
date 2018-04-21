50faef3cce22f0df2f969ed9fb8d125214e101ae
Konye <konyekokim@gmail.com>
2018-04-21 03:52:24 +0100

    added user available checker to start activity to open NoteList if there is a user on the application

--- app/src/main/java/com/chokus/konye/privatememo/activity/GetStartedActivity.kt
+++ app/src/main/java/com/chokus/konye/privatememo/activity/GetStartedActivity.kt
@@ -7,12 +7,14 @@ import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
 import android.widget.Toast
 import com.chokus.konye.privatememo.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 import kotlinx.android.synthetic.main.activity_get_started.*
 import kotlinx.android.synthetic.main.content_get_started.*
 
 class GetStartedActivity : AppCompatActivity() {
-
+    private var firebaseAuth : FirebaseAuth? = null
     companion object {
         const val USER_EMAIL = "com.chokus.konye.privatememo.activity user email address"
     }
@@ -22,6 +24,7 @@ class GetStartedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_get_started)
         setFullScreen()
         viewActions()
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     private fun viewActions(){
@@ -42,6 +45,14 @@ class GetStartedActivity : AppCompatActivity() {
             }else{
                 Toast.makeText(this, "Enter your email  address", Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser : FirebaseUser? = firebaseAuth?.currentUser
+        if(currentUser != null){
+            startActivity(Intent(applicationContext, NoteListActivity::class.java))
         }
     }
 
